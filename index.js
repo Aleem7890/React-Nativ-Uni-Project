ab3c49199e6c8f434a69492cef317e4a2549da21
Aleem7890 <143121195+Aleem7890@users.noreply.github.com>
2023-08-25 11:25:11 +0500

    Added Name and Roll Number
    cannot run the file so I have added comments.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 /**
  * @format
+ * 
+ * Sir, Im Unable to run the app on my device. I tried to run it on my device but it is not working.
+ * 
+ * so kindly accept this as my submission.
+ * 
+ * Name: Aleem Ehsan
+ * Roll: 201370057
  */
 
 import {AppRegistry} from 'react-native';
@@ -7,3 +14,5 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+
